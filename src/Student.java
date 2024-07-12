0559d62baa5d870546e7294ff9213c9ee2fd8e52
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-07-12 07:30:32 +0530

    Add files via upload

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    int studentId;
+    String name;
+    double gpa;
+
+    public Student(int studentId, String name, double gpa) {
+        this.studentId = studentId;
+        this.name = name;
+        this.gpa = gpa;
+    }
+}
